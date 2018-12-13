47a4e1660af54c9805113c57488d36f7fc6bcce9
rachel <hai30@163.com>
2018-12-13 23:04:35 +0800

    Fixed pictures show

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -10,10 +10,15 @@ Route::group([
     'middleware'    => config('admin.route.middleware'),
 ], function (Router $router) {
 
-    $router->get('/', 'HomeController@index');
+    $router->get('/', 'HomeController@index')->name('index');
     $router->resource('users', UserController::class);
     $router->resource('couriers', CourierController::class);
     $router->resource('votes', VoteController::class);
+
+    $router->get('/complaint/change', 'ComplaintController@change')->name('complaint.change');
+
     $router->resource('complaints', ComplaintController::class);
+
+
 
 });
