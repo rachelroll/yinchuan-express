7ff0ffa7116b85b6e64ca38e561b79a82d73cb28
rachel <hai30@163.com>
2018-12-16 15:02:07 +0800

    Update deploy

---
+++ deploy.php
@@ -0,0 +1,91 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// 邮政投票后台-oe服务器
+set('application', 'yinchuan-express');
+
+// Project repository
+set('repository', 'git@git.dev.tencent.com:hai30/yinchuan-express.git');
+set('composer_options', '{{composer_action}} --verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader --ignore-platform-reqs
+');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+set('writable_mode', 'chown');
+
+set('keep_releases', 2);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+
+// Hosts
+host('oeaudio.com')
+    ->user('root')
+    ->set('deploy_path', '/var/www/{{application}}')
+    ->stage('staging');
+
+
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
+/**
+ * Main task
+ */
+desc('Deploy your project');
+
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'artisan:storage:link',
+    'artisan:view:clear',
+    'artisan:optimize',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
+
+
+//task('deploy:upload', function () {
+//    upload('build/', '{{release_path}}/public');
+//});
+
+task('reload:php-fpm', function () {
+    run('service php7.2-fpm restart');
+});
+
+after('deploy', 'reload:php-fpm');
+
+task('restart:supervisorctl', function () {
+    run('supervisorctl restart all');
+});
+
+//after('deploy', 'restart:supervisorctl');
+
+after('deploy', 'success');
+
