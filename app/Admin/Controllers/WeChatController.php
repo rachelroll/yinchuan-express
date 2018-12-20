90da43ffdae6a30c1523963eb9dc6ddf04a39a9a
lixiuye <hai30@163.com>
2018-12-20 19:39:15 +0800

    投诉消息推送完成

--- app/Admin/Controllers/WeChatController.php
+++ app/Admin/Controllers/WeChatController.php
@@ -1,29 +1,31 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Admin\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
 
 class WeChatController extends Controller
 {
     public function serve()
     {
-        dd(37937);
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
 
-        $app->template_message->setIndustry(13, 14);
+        //修改所属行业
+        //$app->template_message->setIndustry(13, 14);
 
+        //$app->template_message->addTemplate('I0SnFsWHK3wkTIAmYghRA45BoTLlxcl1hXZqJRpxa3E');
         $app->template_message->send([
             'touser' => 'oi-uR0ktJyvXx8HP5-5DVHF_kUgQ',
-            'template_id' => 'oeR93ZHiU_fMd21zH9l8XFsa9SyAIQmyajWp-Hw7uxY',
-            'url' => 'https://easywechat.org',
+            'template_id' => 'I0SnFsWHK3wkTIAmYghRA45BoTLlxcl1hXZqJRpxa3E',
             'data' => [
-                'key1' =>    'hao',
-                'key2' => 'hao',
-                'key3' => 'hao',
+                'phone'   => '18513183115',
+                'type'    => '延迟派送',
+                'time'    => '2018年11月25日',
+                'content' => '太晚了'
                 ],
             ]);
     }
