725b7a4da1f5b20184a456a2afcf6be67bf60580
rachel <hai30@163.com>
2019-01-18 01:30:39 +0800

    Update

---
+++ app/Admin/Controllers/CompanyController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Company;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class CompanyController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('Index')
+            ->description('description')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Detail')
+            ->description('description')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit')
+            ->description('description')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Create')
+            ->description('description')
+            ->body($this->form());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Company);
+
+        $grid->id('ID');
+        $grid->mobile('负责人手机号');
+        $grid->manager('负责人姓名');
+        $grid->open_id('负责人微信ID');
+        $grid->company_name('企业名称');
+
+        $status = [
+            'on'  => ['value' => 1, 'text' => '启用', 'color' => 'success'],
+            'off' => ['value' => 0, 'text' => '禁用', 'color' => 'danger'],
+        ];
+        $grid->enabled('是否启用')->switch($status)->sortable();
+
+        $grid->created_at('创建时间');
+        $grid->updated_at('更新时间');
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Company::findOrFail($id));
+
+
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Company);
+
+        $form->text('mobile', '负责人手机号');
+        $form->text('manager', '负责人姓名')->rules('required',['required'=>'必填项']);
+        $form->text('open_id', '负责人微信ID');
+        $form->text('company_name', '企业名称')->rules('required',['required'=>'必填项']);
+
+        $options = [
+            'on'  => ['value' => 1, 'text' => '是', 'color' => 'primary'],
+            'off' => ['value' => 0, 'text' => '否', 'color' => 'default'],
+        ];
+        $form->switch('enabled', '是否启用')->states($options);
+
+        return $form;
+    }
+}
